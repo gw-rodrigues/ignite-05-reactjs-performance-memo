b3c3c284ccc93cded0074214eb766541821c19ec
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-09-08 09:32:21 +0200

    ADD generate fake info

---
+++ server.js
@@ -0,0 +1,17 @@
+//vamos criar server para gerar valores para usar nesta app
+module.exports = ()=>{
+    const data = {
+        products:[]
+    }
+
+    for(let i = 0; i<1000; i++){
+        data.products.push(
+            {
+                id:i+1,
+                price:80,
+                title:`camiseta ${i+1}`
+            }
+        )
+    }
+    return data;
+}
